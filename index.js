2e54b010d063ab6ab52e0ea60759edae2c50222b
tasnimalam001 <tasnimalam.001@gmail.com>
2023-06-13 04:06:57 +0600

    checking admin with email

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
+
+
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlgqora.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -56,6 +77,18 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(filter,updateRole);
+      res.send(result);
+    })
+
+    app.get('/users/admin/:email',verifyJWT,  async(req,res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query= {email:email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
       res.send(result);
     })
 
@@ -105,9 +138,7 @@ async function run() {
     //selected class code
 
 
-
-
-    app.get('/selectedClass', async(req,res) => {
+    app.get('/selectedClass', verifyJWT, async(req,res) => {
       const email = req.query.email;
       console.log(email);
       
@@ -115,10 +146,10 @@ async function run() {
         res.send([]);
       }
 
-      // const decodedEmail = req.decoded.email;
-      // if(email !== decodedEmail){
-      //   return res.status(403).send({error: true, message: 'forbidden access'})
-      // }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbidden access'})
+      }
 
       const query = { email: email };
       const result = await selectedClassCollection.find(query).toArray()
